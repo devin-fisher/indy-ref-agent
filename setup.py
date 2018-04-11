5825ed7ddc826b5a4a7c47f3480284764a094471
Devin Fisher <devin.fisher@evernym.com>
2018-04-10 18:25:25 -0600

    inital commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='indy-ref-agent',
+    version='0.1.0',
+    description='reference implementation of agent protocol',
+    long_description=readme,
+    author='Devin Fisher',
+    author_email='devin.fisher@evernym.com',
+    packages=find_packages(exclude=('tests', 'docs')),
+    include_package_data=True
+)
---
+++ tests/test_noop.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestCxsInit(unittest.TestCase):
+
+    def test_noop(self):
+        # Makes sure we can run tests
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
